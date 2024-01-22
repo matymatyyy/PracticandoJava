e9c37306509680c0882309d1ef28bd93eaea35d8
Matias Dominguez <matydominguez554@gmail.com>
2024-01-22 14:05:35 -0300

    Codigo por funciones

--- clase5/src/Main.java
+++ clase5/src/Main.java
@@ -3,28 +3,25 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
-        Scanner ingresoDato= new Scanner(System.in);
-        int menor = 0;
-        int mayor = 0;
-        float suma= 0;
         int[] lista = new int[5];
+        Scanner ingresoDato= new Scanner(System.in);
 
-        for(int i=0; i < lista.length; i++){
-            System.out.print("Ingrese el numero: ");
-            int dato = ingresoDato.nextInt();
-            lista[i] = dato;
-            if(mayor< dato || i==0){
-                    mayor=dato;
-            }
-            if(menor>dato || i==0){
-                    menor=dato;
-            }
-            suma += dato;
+        ingresarNumeros(lista, ingresoDato);
+        mostrarNumeros(lista);
         }
+
+    private static void ingresarNumeros(int[] lista, Scanner ingresoDato) {
+        for(int i = 0; i< lista.length; i++){
+            lista[i]= pedirNumero(ingresoDato);;
+        }
+    }
+
+    private static void mostrarNumeros(int[] lista){
         System.out.println(Arrays.toString(lista));
-        System.out.println("El numero mas grande es: "+ mayor+ " El numero mas chiquito es :"+ menor);
-        System.out.println("El promedio es :"+ (suma/ lista.length));
-
-        }
+    }
+    private static int pedirNumero(Scanner ingresoDato){
+        System.out.print("Ingrese un numero:");
+        int dato = ingresoDato.nextInt();
+        return dato;
+    }
     }
