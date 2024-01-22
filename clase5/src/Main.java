4d53e03d8765430a63797c3225de22b7b9d356ad
Matias Dominguez <matydominguez554@gmail.com>
2024-01-22 11:04:19 -0300

    Probando bucles y Arrays

--- clase5/src/Main.java
+++ clase5/src/Main.java
@@ -5,16 +5,26 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner ingresoDato= new Scanner(System.in);
-        int contador = 0;
+        int menor = 0;
+        int mayor = 0;
+        float suma= 0;
         int[] lista = new int[5];
 
-        do {
-            System.out.print(" Ingrese un numero: ");
-            Integer numero = ingresoDato.nextInt();
-            lista[contador] = numero;
-            contador++;
-        } while (contador != lista.length);
+        for(int i=0; i < lista.length; i++){
+            System.out.print("Ingrese el numero: ");
+            int dato = ingresoDato.nextInt();
+            lista[i] = dato;
+            if(mayor< dato || i==0){
+                    mayor=dato;
+            }
+            if(menor>dato || i==0){
+                    menor=dato;
+            }
+            suma += dato;
+        }
         System.out.println(Arrays.toString(lista));
+        System.out.println("El numero mas grande es: "+ mayor+ " El numero mas chiquito es :"+ menor);
+        System.out.println("El promedio es :"+ (suma/ lista.length));
 
         }
     }
